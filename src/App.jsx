2806d4b3cae79f6094a716160a888204a5a35506
Sharmin64 <lizaeatsbanana@gmail.com>
2023-08-26 13:34:55 +0600

    toaster installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import "./App.css";
+/* eslint-disable react/no-unknown-property */
+import React, {useEffect, useState} from "react";
 import AboutMe from "./components/AboutMe";
 import Banner from "./components/Banner";
 import ContactMe from "./components/ContactMe";
@@ -7,22 +8,50 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Services from "./components/Services";
+import {PropagateLoader} from "react-spinners";
+import logo from "/MLogo.png";
+import {motion} from "framer-motion";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <div>
-        <Header />
-        <Banner />
-        <Navbar />
-        <Services />
-        <Projects />
-        <AboutMe />
-        <ContactMe />
-        <Footer />
-      </div>
+      {loading ? (
+        <div className="flex flex-col justify-center items-center mt-[350px]">
+          <motion.div
+            animate={{y: [20, 50, 20], opacity: 1, scale: 0.8}}
+            transition={{
+              duration: 1.5,
+              delay: 0.1,
+              ease: [0.5, 0.3, 1, 1.5],
+            }}
+            initial={{opacity: 0, scale: 0.5}}
+            whileHover={{scale: 0.2}}
+          >
+            <img src={logo} alt="Logo" />
+          </motion.div>
+          <PropagateLoader size={25} color="#07a8D1" />
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Banner />
+          <Navbar />
+          <Services />
+          <Projects />
+          <AboutMe />
+          <ContactMe />
+          <Footer />
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default App;
